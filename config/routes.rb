2cc0c67981063a61c6e04146a3b7f60c1cfeb6db
kajalbhardwaj <kabharadwaj@bestpeers.com>
2021-09-29 18:50:54 +0530

    resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  root 'pages#home'
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+   resources :posts, only: [:index, :show, :create] do
+    resources :photos, only: [:create]
+    resources :comments, only: [:create, :destroy]
+    resources :likes, only: [:create, :destroy]
+end
 end
