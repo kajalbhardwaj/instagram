77359792f41db9df262522f38dc7c5d47a774fac
kajalbhardwaj <kabharadwaj@bestpeers.com>
2021-10-06 21:26:54 +0530

    add pagination

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,50 +1,54 @@
 class PostsController < ApplicationController
-    #before_action :authenticate_user
-    def index
-        @posts = Post.all
-    end
-    
-    def new
-        @post = Post.new
-      end
-    
-    def create
-        @post =  Post.create(post_params)
-        if @post.save
-         redirect_to @post
-        else
-            render :new
-        end 
-    end
-    
-    def show
-        @post = Post.find(params[:id])
-    end
+  #before_action :authenticate_user
+  def index
+    #@posts = Post.all
+    @posts = Post.paginate(page: params[:page], per_page: 10)
+   end
 
-    def edit
-        @post = Post.find(params[:id])
-    end
+  def new
+    @post = Post.new
+   end
+
+   def create
+    @post =  Post.create(post_params)
+    @post.user = current_user
+    if @post.save
+     redirect_to @post, notice: 'post was succesfully created'
+    else
+      render :new
+    end 
+   end
+
+   def show
+    @post = Post.find(params[:id])
+  end
+
+   def edit
+    @post = Post.find(params[:id])
+   end
+
+   def update
+    @post = Post.find(params[:id])
+
+    if @post.update(post_params)
+      redirect_to @post
     
-    def update
-        @post = Post.find(params[:id])
-    
-        if @post.update(post_params)
-          redirect_to @post
-        
-        end
     end
-    def destroy
-        @post = current_user.posts.find(params[:id])
-        @post.destroy
-        
-        redirect_to user_path(current_user)
-    end
-    
-    private
-    
-    def post_params
-        params.require(:post).permit(:conetent, :title, :image, :user_id)
-    end
+  end
+  
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+
+    #redirect_to root_path
+    redirect_to posts_url, notice: 'Post was successfully destroyed.' 
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:conetent, :image, :title, :user_id, :avatar)
+   end
 end
- 
- 
+
+
