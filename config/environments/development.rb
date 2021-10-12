c5a0a5bac72e10636c7a00f954f9563fb9d3b2bf
kajalbhardwaj <kabharadwaj@bestpeers.com>
2021-10-12 19:19:26 +0530

    add action maler

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,10 +33,12 @@ Rails.application.configure do
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
+  
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
+  
 
-  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_caching = true
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
@@ -74,5 +76,18 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from:'no-reply@example.com'}
 
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            'bhardwajkajal1999@gmail.com',
+    password:             '7078367629',
+    authentication:       'plain',
+    enable_starttls_auto: true }
 end
